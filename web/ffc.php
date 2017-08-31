4353c3a03ea3fa26a6048b38f3b0b475e924d4b4
veryrich <mail@zhutingyu.com>
2017-08-31 22:58:39 +0800

    version0.4

--- web/ffc.php
+++ web/ffc.php
@@ -1,4 +1,5 @@
 <?
+
 // $lastNo=$this->getGameLastNo(5);
 // $api = 'http://www.qqfenfencai.com/open/code_new.asp';
 // $result = file_get_contents( $api );  
@@ -23,8 +24,7 @@
 // echo '<xml>
 // <row expect="'.$actionNo.'" opencode="'.$realCode.'" opentime="'.$lastNo['actionTime'].'"/>
 // </xml>';
-ob_start();
-$lastNo=$this->getGameLastNo(5);
+//$lastNo=$this->getGameLastNo(5);
 $api = 'http://www.77tj.org/api/tencent/onlineim';
 $resultJson = file_get_contents( $api ); 
 if(!$resultJson)exit;
@@ -35,7 +35,7 @@ $total = 0;
 for($i = 0; $i < strlen($onlineNumber); $i++) $total += (int)$onlineNumber[$i]; 
 $lastNumber = substr($onlineNumber, -4);
 if(strlen($lastNumber) < 4) exit;
-$openCode = substr($total, -1).$lastNumber;
+$openCode = $lastNumber.substr($total, -1);
 $realCode = "";
 for($i = 0; $i < strlen(trim($openCode)); $i++) {
     $realCode .= $openCode[$i].",";
@@ -44,10 +44,8 @@ $realCode = rtrim($realCode, ',');
 $data['opencode'] = $realCode;
 $data['opentime'] = $ffc['onlinetime'];
 #var_dump($lastNo);exit(0);
-header('Content-type: application/xml');
-echo'<?xml version="1.0" encoding="utf-8"?>';
+header('Content-Type: text/xml;charset=utf8');
+$lastNo['actionTime']= 111;
 echo '<xml>
-<row expect="'.$lastNo['actionNo'].'" opencode="'.$realCode.'" opentime="'.$ffc['onlinetime'].'"/>
+<row expect="'.$lastNo['actionTime'].'" opencode="'.$realCode.'" opentime="'.$ffc['onlinetime'].'"/>
 </xml>';
-
-ob_end_flush();
